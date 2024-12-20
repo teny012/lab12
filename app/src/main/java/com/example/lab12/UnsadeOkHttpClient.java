4a580095a943b359d1d329053d1aade84fa408a2
teny012 <111360143@ntut.org.tw>
2024-12-20 16:02:54 +0800

    1

---
+++ app/src/main/java/com/example/lab12/UnsadeOkHttpClient.java
@@ -0,0 +1,10 @@
+package com.example.lab12;
+
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import 
+
+public class UnsadeOkHttpClient {
+}
